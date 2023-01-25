b7fd6b7d9f305b43fe25ccbde035bdf11b845821
joao-rcosta19 <joao.rcosta19@gmail.com>
2023-01-25 18:27:18 -0300

    atualização

--- api/app.py
+++ api/app.py
@@ -6,6 +6,8 @@ from infra.repositorios.repositoriosFuncs import validaCampo, validaCampoEmail, 
 
 app = Flask(__name__, template_folder="public")
 
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -14,7 +16,7 @@ def index():
 def erro():
     return render_template('erro.html')
 
-@app.route('/validar', methods=['POST'])
+@app.route('/validar', methods=['GET','POST'])
 def validaChave():
     if request.method == 'POST':
         chave = request.form['chave']
@@ -26,7 +28,7 @@ def validaChave():
         if validar == True:
             frase = " "
             if validaCampo(email) == False:
-                return {'status': 'Nao Enviado! verifique se o campo endereco de email esta vazio!', 'endereco': email}
+                return json.dumps({'status': 'Nao Enviado! verifique se o campo endereco de email esta vazio!', 'endereco': email})
             elif frase not in email:
                 a = buscar(email, "lista_email2", "email")
             else:
@@ -48,17 +50,17 @@ def validaChave():
             if validaCampo(assunto) == True and validaCampo(corpo) == True:
                 if buscarsituacao(email, "lista_email2", "email", 2) == True:
                     enviaremail(assunto, corpo, email)
-                    return {'chave': chave, 'assunto do email': assunto, 'corpo do email': corpo, 'destinatario': email, 'status': 'Enviado!'}
+                    return json.dumps({'chave': chave, 'assunto do email': assunto, 'corpo do email': corpo, 'destinatario': email, 'status': 'Enviado!'})
                     #print("Email enviado!")
                 else:
-                    return {'status': 'Nao Enviado! verifique o endereco de email...', 'destinatario': email}
+                    return json.dumps({'status': 'Nao Enviado! verifique o endereco de email...', 'destinatario': email})
             elif validaCampo(assunto) == False:
-                return {'status': 'Nao Enviado! verifique se o campo Assunto esta vazio!', 'assunto do email': assunto}
+                return json.dumps({'status': 'Nao Enviado! verifique se o campo Assunto esta vazio!', 'assunto do email': assunto})
             elif validaCampo(corpo) == False:
-                return {'status': 'Nao Enviado! verifique se o campo Corpo do email esta vazio!', 'corpo do email': corpo}
+                return json.dumps({'status': 'Nao Enviado! verifique se o campo Corpo do email esta vazio!', 'corpo do email': corpo})
             else:
-                return {'status': 'Nao Enviado! verifique se o campo Assunto e Corpo do email estao vazio!', 'assunto do email': assunto , 'corpo do email': corpo}
-        return {'status': 'Nao Enviado! verifique sua chave!', 'chave inserida': chave} #redirect(url_for('erro'))
+                return json.dumps({'status': 'Nao Enviado! verifique se o campo Assunto e Corpo do email estao vazio!', 'assunto do email': assunto , 'corpo do email': corpo})
+        return json.dumps({'status': 'Nao Enviado! verifique sua chave!', 'chave inserida': chave}) #redirect(url_for('erro'))
 
 
 
